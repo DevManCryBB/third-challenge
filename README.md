d3e660445370b272073434f671bdb7c0fd225bd0
DevManCryBB <veronicagriggs3@gmail.com>
2023-03-30 18:43:18 -0700

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# third-challenge
-Third Module Challenge
+
+# Third Module Challenge
+ 
+ My motivation for creating this project was to allow users to create a secure random password with ease. I learned how to build a random password generator.
+
+## Usage
+
+To use this application simply follow the prompts to make your selections and once all of the prompts are finished, click the "Generate Password" button.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+https://github.com/Ashlhc
+https://github.com/mercurybased
+https://github.com/17anguyen?tab=repositories
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -9,91 +10,195 @@ function writePassword() {
 }
 
 //variables
-var upperCase = Array.from({length: 26}, (_,i) => String.fromCharCode("A".fromCharCodeAt(0) + i));
-var lowerCase = Array.from({length: 26}, (_,i) => String.fromCharCode("a".fromCharCodeAt(0) + i));
-var nums = "1234567890"
-var characterSpecial = ["!", "@", "#", "$", "%"]
+
+var lowerCaseArray = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var upperCaseArray = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var nums = ["1", "2", "3", "4", "5" , "6", "7", "8", "9", "0"];
+var characterSpecial = ["!", "@", "#", "$", "%"];
+var availableChar = []
 
 //make generatePassword function
-function generatePassword()
-
-
-//prompt for pw length
-var pwdLength =
-prompt("How long would you like your password to be (enter a number)");
-console.log (pwdLength)
-
-if (pwdLength >=8 && pwdLength <= 128) {
-  alert("Great!")
-}
-
-//if statement if number is above 8 and below 128
-if (num > 8)
-  console.log("try again")
-else if (num < 128)
-  console ("try again")
-
-//confirm for lowercase
-
-
-// function lowercase() {
-  var userChoiceLow = confirm("Would you like to use lowercase letters?");
-  {
-  if (userChoiceLow == true)
-  {
-    alert("nice, lowercase letters it is")
-    console.log(userChoiceLow)
-  } else if (userChoiceLow == false); {
-    alert("alright, no lowercase");
+function getUserChoice() {
+  //prompt for pw length
+  var pwdLength = prompt(
+    "How long would you like your password to be (enter a number)"
+    );
+    console.log(pwdLength);
+    
+    if (pwdLength >= 8 && pwdLength <= 128) {
+      alert("Great!");
+    }
+      
+     // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (Number.isNaN(pwdLength)) {
+    alert('Password length must be provided as a number');
+    return null;
   }
-}
 
-var userChoiceUp = confirm("Would you like to use lowercase letters?");
-{
-if (userChoiceUp == true)
-{
-  alert("nice, uppercase letters it is")
-  console.log(userChoiceUp)
-} else if (userChoiceUp == false); {
-  alert("alright, no uppercase");
-  console.log (userChoiceUp)
-}
-}
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (pwdLength < 8) {
+    alert('Password length must be at least 8 characters');
+    return null;
+  }
 
-var randomLower = lowerCase [randomInput]
-var randomInput = Math.floor(Math.random() * pwdLength.length);
-  var randomLower = lowerCase[randomInput]
-    console.log (randomLower)
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (pwdLength > 128) {
+    alert('Password length must less than 129 characters');
+    return null;
+  }
+    
+    // confirm lowercase() {
+      var userChoiceLow = confirm("Would you like to use lowercase letters?");
+      {
+        if (userChoiceLow) {
+          alert("nice, lowercase letters it is");
+          // add lowercase letters to avail char array (concat)
+          console.log(userChoiceLow);
+        } else{
+          alert("alright, no lowercase");
+        }
+      }
+      
+      var userChoiceUp = confirm("Would you like to use uppercase letters?");
+      {
+        if (userChoiceUp == true) {
+          alert("nice, uppercase letters it is");
+          // add uppercase letters to avail char array (concat)
+          console.log(userChoiceUp);
+        } else {
+          
+          alert("alright, no uppercase");
+          console.log(userChoiceUp);
+        }
+      }
+      
+      var userChoiceSpec = confirm("Would you like to use special characters?");
+      {
+        if (userChoiceSpec == true) {
+          alert("nice, special characters it is");
+          // add specials to availchar
+          console.log(userChoiceSpec);
+        } else {
+          alert("alright, no uppercase");
+          console.log(userChoiceSpec);
+        }
+      }
+      var userNum = confirm("Would you like to use numbers?");
+      {
+        if (userNum == true) {
+          alert("nice, numbers it is");
+          console.log(userNum);
+        } else {
+          alert("alright, no uppercase");
+          console.log(userNum);
+        }
+      }
+      //store their response in a variable
+      
+        var answers = {
+           length: pwdLength, 
+           lowerCase: userChoiceLow,
+           upperCase: userChoiceUp,
+           specChar: userChoiceSpec,
+           numbers: userNum,
+        }
+        console.log("answers", answers)
+        return answers
+      }
+    
+    // Function for getting a random element from an array
+    function getRandom(arr) {
+      var randIndex = Math.floor(Math.random() * arr.length);
+      var randElement = arr[randIndex];
+    
+      return randElement;
+    }
 
-//store their response in a variable called characterCase
+    function generatePassword() {
+      var userChoice = getUserChoice()
+      
+      if (userChoice.lowerCase){
+        availableChar = availableChar.concat(lowerCaseArray)
+      }
+      if (userChoice.upperCase){
+        availableChar = availableChar.concat(upperCaseArray)
+      }
+      if (userChoice.numbers){
+        availableChar = availableChar.concat(nums)
+      }
+      if (userChoice.specChar){
+        availableChar = availableChar.concat(characterSpecial)
+      }
+      console.log("New Array", availableChar)
+      var password = pickCharacters(availableChar, userChoice.length)
+      return password
+    }
 
-var characterCase = 
-
-//if else statements checking each confirm and if it yes or true , push or add random characters to a big array of all characters selected so far
-
-//for loop while i is less than their answer for num length prompt
-
-//in this for loop we would randomly pull characters from the big array
-
-//pull random characters from array using math.floor(math.random)*array.length
-
-//have a var declared above for loop 
-
-//that var += theRandomChar
-
-//Return the pw var
-
-//EDGE cases: picking no for every prompt
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+     //for loop while i is less than their answer for num length promp
+    function pickCharacters (array, length){
+      var userPassword = []
+      for (var i = 0; i <length; i++)
+      {
+      var passwordChar = getRandom(array)
+      userPassword.push(passwordChar)
+      }
+      var result = userPassword.join("")
+      return result
+    }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
